c158d0176c4c2d4ca6a9664cc0dc6933e2b28248
Zekiri abdelali <zekiriabd@yahoo.fr>
2018-09-20 20:58:23 +0200

    Popup and Alert in Delphi FMX

---
+++ PopUp_Alert/Project5.dpr
@@ -0,0 +1,14 @@
+program Project5;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  Unit5 in 'Unit5.pas' {Form5};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm5, Form5);
+  Application.Run;
+end.
