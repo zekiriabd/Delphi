93b9801fc61ef595b0402543d5308c91eedcf0e4
Zekiri abdelali <zekiriabd@yahoo.fr>
2018-09-23 22:27:40 +0200

    Notification Id Delphi FMX

---
+++ Notification/Local_Notification/Unit5.pas
@@ -0,0 +1,49 @@
+﻿unit Unit5;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  System.Notification, FMX.Controls.Presentation, FMX.StdCtrls;
+
+type
+  TForm5 = class(TForm)
+    Button1: TButton;
+    NotificationCenter1: TNotificationCenter;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form5: TForm5;
+
+implementation
+    uses
+    FMX.Platform;
+{$R *.fmx}
+
+procedure TForm5.Button1Click(Sender: TObject);
+var
+  MyNot : TNotification;
+begin
+
+Try
+  MyNot := NotificationCenter1.CreateNotification();
+  MyNot.Name := 'MyNot';
+  MyNot.Title:= 'عنوان الرسالة';
+  MyNot.AlertBody:= 'موضوع الرسالة';
+  NotificationCenter1.PresentNotification(MyNot);
+Finally
+  MyNot.DisposeOf;
+End;
+
+
+
+
+
+end;
+end.
